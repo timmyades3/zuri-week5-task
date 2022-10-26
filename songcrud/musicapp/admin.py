1fc8e38be953afddab98deb2741a1cd128656546
timmyades3 <oluwatimileyinadesina623@gmail.com>
2022-10-26 18:46:54 +0100

    initial commit

---
+++ songcrud/musicapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Artiste, Lyric, Song
+
+# Register your models here.
+
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
